758eb3c2be3183cc5daa1cc86203532b09120172
val123-e14 <127456387+val123-e14@users.noreply.github.com>
2023-12-07 17:01:19 -0300

    Update guardar_pedido.php

--- guardar_pedido.php
+++ guardar_pedido.php
@@ -14,16 +14,30 @@ if ($conn->connect_error) {
 }
 
 if(isset($_POST['cantidad'])) {
-    $cantidad = $_POST['cantidad'];
+    // Utilizar sentencias preparadas para prevenir la inyección de SQL
+    $cantidad = mysqli_real_escape_string($conn, $_POST['cantidad']);
 
-    // Consulta para insertar solo en el campo cantidad en la tabla pedidos
-    $sql = "INSERT INTO pedidos (cantidad) VALUES ('$cantidad')";
+    // Consulta preparada para insertar en la tabla pedidos
+    $sql = "INSERT INTO pedidos (cantidad) VALUES (?)";
+    $stmt = $conn->prepare($sql);
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Cantidad insertada correctamente en la tabla pedidos";
+    if ($stmt) {
+        // Asociar parámetros y ejecutar la consulta
+        $stmt->bind_param("s", $cantidad);
+
+        if ($stmt->execute()) {
+            echo "Cantidad insertada correctamente en la tabla pedidos";
+        } else {
+            echo "Error al insertar la cantidad en la tabla pedidos: " . $stmt->error;
+        }
+
+        // Cerrar la consulta preparada
+        $stmt->close();
     } else {
-        echo "Error al insertar la cantidad en la tabla pedidos: " . $conn->error;
+        echo "Error en la preparación de la consulta: " . $conn->error;
     }
+} else {
+    echo "La cantidad no está definida en el formulario.";
 }
 
 // Cerrar la conexión a la base de datos
